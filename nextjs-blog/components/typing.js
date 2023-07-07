553e12719823a63646dcfb66d7d9b27fa295e2d5
Nunzio Ruffo <nruffo@zilliant.com>
2023-07-07 15:22:44 -0500

    initial nextjs files

---
+++ nextjs-blog/components/typing.js
@@ -0,0 +1,15 @@
+function Typing() {
+    return (
+        <>
+            <p id="offscreen-text" className="offscreen-text"></p>
+            <p id="text" className="text"></p>
+
+            <svg id="svg">
+            </svg>
+
+            <input type="text" className="input" id="input" />
+        </>
+    )
+}
+
+export default Typing
